0dec08791152fe6ef68b1a86d4b69c446ad93431
Matti Ben-Avraham <matti@Ben-Avraham.info>
2013-05-19 16:54:18 +0300

    Fixed error (hopfully)

--- EventApi/DataSources/MongoDbManager.cs
+++ EventApi/DataSources/MongoDbManager.cs
@@ -1,6 +1,8 @@
 ﻿using EventApi.Models;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
+using MongoDB.Bson;
+using MongoDB;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -33,10 +35,16 @@ namespace EventApi.DataSources
             //{
             //    Database.RunCommand(
             //}
+            //var mongo = new Mongo();
+            //mongo.Connect();
+            //var db = mongo.getDB("movieReviews");
+            
             var connectionString = GetMongoDbConnectionString();// "mongodb://localhost";
             var client = new MongoClient(connectionString);
+            //client.
             var server = client.GetServer();
             var database = server.GetDatabase(m_eventsDataBaseName);
+            
             var collection = database.GetCollection<MongoDbEvent>(m_eventsDataBaseName);
 
             collection.RemoveAll();
@@ -60,11 +68,29 @@ namespace EventApi.DataSources
 
         public MongoCollection<MongoDbEvent> GetEventsCollection()
         {
+            MongoUrl mongoUrl = new MongoUrl(ConfigurationManager.AppSettings.Get("MONGOLAB_URL"));
             var connectionString = GetMongoDbConnectionString();// "mongodb://localhost";
-            var client = new MongoClient(connectionString);
-            var server = client.GetServer();
-            var database = server.GetDatabase(m_eventsDataBaseName);
-            return database.GetCollection<MongoDbEvent>(m_eventsDataBaseName);
+
+            var database = MongoDatabase.Create(mongoUrl);
+            //var client = new MongoClient(mongoUrl);
+
+            
+            
+            //var server = client.GetServer();
+
+            //server.Connect();
+
+            //var database = server.GetDatabase("appharbor_a43b5174-86b3-4950-a8e8-01e79ff5dcc9");
+            //if (database == null)
+            //{
+            //    server.CreateDatabaseSettings(m_eventsDataBaseName);
+            //}
+            var collections = database.GetCollection<MongoDbEvent>(m_eventsDataBaseName);
+            //collections.RemoveAll();
+            var tt = collections.FindAll();
+            //server.Disconnect();
+
+            return collections;
 
         }
 
